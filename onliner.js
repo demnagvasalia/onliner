f862510d8b4b5c2d80b501274a7266fda3cb0f4d
Administrator <aeronporter@gmail.com>
2024-01-03 15:38:01 -0800

    fix

--- onliner.js
+++ onliner.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
-const Eris = require('eris')
-const tokens = fs.readFileSync('tokens.txt', 'utf-8').split('\n').filter(Boolean);
+const Eris = require('eris');
+const tokens = fs.readFileSync('tokens.txt', 'utf-8').split('\n').map(token => token.trim()).filter(Boolean);
 
 const bots = [];
 for (const token of tokens) {
+    console.log(token);
     const bot = new Eris(token);
     bot.connect();
     bots.push(bot);
